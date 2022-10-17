4a784775e768d276523d24ac5a96c4c2112c16ca
utimateman <krai020913@gmail.com>
2022-10-17 22:54:09 +0700

    add time

--- example_data_pipeline/run_this_execution_script.py
+++ example_data_pipeline/run_this_execution_script.py
@@ -1,5 +1,5 @@
 import os
-
+import time
 
 # [ User Level Data ] - Raw -> Processed
 os.system("python3 1_2_create_raw_user_level_data.py") 
@@ -12,26 +12,32 @@ insert_data_scripts = ['1_1_create_raw_user_data','2_1_insert_raw_data','2_2_ins
 
 for insert_data_script in insert_data_scripts:
     os.system("python3" + insert_data_script + ".py") 
+    time.sleep(3)
 
-    # # Update Pre-Processed Data Table 
-    # os.system("python3 3_create_preprocess_data.py") 
+    # Update Pre-Processed Data Table 
+    os.system("python3 3_create_preprocess_data.py") 
+    time.sleep(3)
 
-    # # Create/Insert/Update Processed Data
-    # os.system("python3 4_1_process_user_data.py") 
-    # os.system("python3 4_2_process_user_data.py") # update using spark sql delta
+    # Create/Insert/Update Processed Data
+    os.system("python3 4_1_process_user_data.py") 
+    time.sleep(3)
 
-    # # Merge Processed Data & Update Final Data
-    # os.system("python3 5_merge_table.py") 
+    os.system("python3 4_2_process_user_data.py") # update using spark sql delta
+    time.sleep(3)
 
-# Update Pre-Processed Data Table 
-os.system("python3 3_create_preprocess_data.py") 
+    # Merge Processed Data & Update Final Data
+    os.system("python3 5_merge_table.py") 
+    time.sleep(3)
 
-# Create/Insert/Update Processed Data
-os.system("python3 4_1_process_user_data.py") 
-os.system("python3 4_2_process_user_data.py") # update using spark sql delta
+# # Update Pre-Processed Data Table 
+# os.system("python3 3_create_preprocess_data.py") 
 
-# Merge Processed Data & Update Final Data
-os.system("python3 5_merge_table.py") 
+# # Create/Insert/Update Processed Data
+# os.system("python3 4_1_process_user_data.py") 
+# os.system("python3 4_2_process_user_data.py") # update using spark sql delta
+
+# # Merge Processed Data & Update Final Data
+# os.system("python3 5_merge_table.py") 
 
 # Print All Table
 os.system("python3 6_get_all_table.py") 
