a851e524769851e0a3fdf05092b76f737f9e8c73
utimateman <krai020913@gmail.com>
2022-10-17 22:47:52 +0700

    update flow of execution for demo

--- example_data_pipeline/run_this_execution_script.py
+++ example_data_pipeline/run_this_execution_script.py
@@ -13,15 +13,25 @@ insert_data_scripts = ['1_1_create_raw_user_data','2_1_insert_raw_data','2_2_ins
 for insert_data_script in insert_data_scripts:
     os.system("python3" + insert_data_script + ".py") 
 
-    # Update Pre-Processed Data Table 
-    os.system("python3 3_create_preprocess_data.py") 
+    # # Update Pre-Processed Data Table 
+    # os.system("python3 3_create_preprocess_data.py") 
 
-    # Create/Insert/Update Processed Data
-    os.system("python3 4_1_process_user_data.py") 
-    os.system("python3 4_2_process_user_data.py") # update using spark sql delta
+    # # Create/Insert/Update Processed Data
+    # os.system("python3 4_1_process_user_data.py") 
+    # os.system("python3 4_2_process_user_data.py") # update using spark sql delta
 
-    # Merge Processed Data & Update Final Data
-    os.system("python3 5_merge_table.py") 
+    # # Merge Processed Data & Update Final Data
+    # os.system("python3 5_merge_table.py") 
+
+# Update Pre-Processed Data Table 
+os.system("python3 3_create_preprocess_data.py") 
+
+# Create/Insert/Update Processed Data
+os.system("python3 4_1_process_user_data.py") 
+os.system("python3 4_2_process_user_data.py") # update using spark sql delta
+
+# Merge Processed Data & Update Final Data
+os.system("python3 5_merge_table.py") 
 
 # Print All Table
 os.system("python3 6_get_all_table.py") 
